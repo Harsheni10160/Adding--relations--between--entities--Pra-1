f1ebc80f6d3992a2490075cc32db99aefa6500c6
harsheni10160 <devaharsheni2006@gmail.com>
2025-04-02 12:26:28 +0530

    completed

---
+++ DestinationCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './DestinationCard.css';
+
+const DestinationCard = ({ destination }) => {
+  const { name, location, image, description, price } = destination;
+
+  return (
+    <div className="destination-card">
+      <img src={image} alt={name} className="destination-image" />
+      <div className="destination-info">
+        <h2 className="destination-name">{name}</h2>
+        <p className="destination-location">{location}</p>
+        <p className="destination-description">{description}</p>
+        <p className="destination-price">{price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DestinationCard; 
